f543b5de2e6a34a3e1acd2f9d5c173fdd6f50261
Camillarhi <rhitanene@gmail.com>
2021-10-15 11:03:38 +0100

    i tested commit all by adding some space

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -12,11 +12,15 @@ namespace inandout.Models
         [Key]
         public int Id { get; set; }
 
-        [DisplayName("My Expenditures")]
+        [DisplayName("My Expenditures")]   
         [Required]
+
         public string Expenditure { get; set; }
         [Required]
         [Range(1, int.MaxValue, ErrorMessage="Amount must be greater than 0!")]
         public int Amount { get; set; }
     }
+
+
+
 }
